e2de07901ce64a5e62b1b6c9ccf5991f51a769dc
Xun <xunchen369@gmail.com>
2017-06-15 02:14:38 -0700

    refactored code

---
+++ src/client/components/sound.js
@@ -0,0 +1,5 @@
+import bgm from "./assets/MeteorSim/bgm.mp3";
+
+export default function(){
+  //TODO load sound here
+}
--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,5 @@
 import * as PIXI from "pixi.js";
 import "./assets/scss/main.scss";
-import bgm from "./assets/MeteorSim/bgm.mp3";
 import Menu from "./assets/MeteorSim/menu_s.png";
 import Backdrop from "./assets/MeteorSim/backdrop.png";
 import Rockets from "./assets/MeteorSim/rocket.png";
@@ -9,6 +8,8 @@ import { PixiSpriteUtil } from "pixiUtils";
 import initialize from "./lifecycle/initialize.js";
 import setup from "./lifecycle/setup.js";
 import run from "./lifecycle/run.js";
+
+import sound from "./components/sound.js"
 
 (function(){
   let state = {
@@ -32,6 +33,7 @@ import run from "./lifecycle/run.js";
     Rockets
   ]).on("progress",loadProgressHandler)
   .load(()=>{
+    sound();
     setup(state);
     run(state);
   });
